8729390b8fbbfefa63f0cb9bce34a202f6f670a4
Jamie <jamie.yjchen@gmail.com>
2021-07-05 09:36:55 -0700

    add gitignore

---
+++ Lab1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab1
+//
+//  Created by Jamie Chen on 2021-07-05.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
